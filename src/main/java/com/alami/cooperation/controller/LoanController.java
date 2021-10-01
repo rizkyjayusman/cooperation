83a07cbb22a73ed9090df7f25875c3f09701e7b0
rizky <rizky.jayusman96@gmail.com>
2021-10-01 07:48:50 +0700

    add requestmapping for loan and add query get total saving

--- src/main/java/com/alami/cooperation/controller/LoanController.java
+++ src/main/java/com/alami/cooperation/controller/LoanController.java
@@ -3,9 +3,11 @@ package com.alami.cooperation.controller;
 import com.alami.cooperation.controller.request.TransactionRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("loans")
 public class LoanController {
 
     // create loan transaction
@@ -13,7 +15,7 @@ public class LoanController {
     public void createLoanTransaction(@RequestBody TransactionRequest transactionRequest) {}
 
     // create pay the loan transaction
-    @PostMapping
+    @PostMapping("pay")
     public void createPayLoanTransaction(@RequestBody TransactionRequest transactionRequest) {}
 
 
--- src/main/java/com/alami/cooperation/repository/SavingRepository.java
+++ src/main/java/com/alami/cooperation/repository/SavingRepository.java
@@ -2,11 +2,13 @@ package com.alami.cooperation.repository;
 
 import com.alami.cooperation.entity.Saving;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 
 public interface SavingRepository extends JpaRepository<Saving, Long> {
 
+    @Query("select count(s.amount) as total_saving from savings s")
     BigDecimal getTotalSaving();
 
     Saving getByMemberId(Long memberId);
