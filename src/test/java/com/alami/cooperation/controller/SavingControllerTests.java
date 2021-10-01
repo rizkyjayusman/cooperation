07a7cb263c00f91321a03d8769c14b5c6c9c1f59
rizky <rizky.jayusman96@gmail.com>
2021-10-01 15:14:53 +0700

    add test case create debit transaction without param

--- src/test/java/com/alami/cooperation/controller/SavingControllerTests.java
+++ src/test/java/com/alami/cooperation/controller/SavingControllerTests.java
@@ -113,4 +113,43 @@ public class SavingControllerTests {
         verify(savingService, times(1)).createDebitTransaction(refEq(transactionDto));
     }
 
+    @Test
+    public void createDebitTransactionWithoutMemberId_shouldReturnHttp400_givenFailureMemberIdNotFound() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest();
+        transactionRequest.setAmount(new BigDecimal(1000000));
+        transactionRequest.setTransactionDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-10"));
+
+        mockMvc.perform(post("/savings/debit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(transactionRequest))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createDebitTransactionWithoutAmount_shouldReturnHttp400_givenFailureAmountNotFound() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest();
+        transactionRequest.setMemberId(1L);
+        transactionRequest.setTransactionDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-10"));
+
+        mockMvc.perform(post("/savings/debit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(transactionRequest))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createDebitTransactionWithoutTransactionDate_shouldReturnHttp400_givenFailureTransactionDateNotFound() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest();
+        transactionRequest.setMemberId(1L);
+        transactionRequest.setTransactionDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-01-10"));
+
+        mockMvc.perform(post("/savings/debit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(transactionRequest))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
 }
