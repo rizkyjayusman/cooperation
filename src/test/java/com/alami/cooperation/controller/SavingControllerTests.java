93ac98e3b96b3f3706751870e9029b62ccf46d40
rizky <rizky.jayusman96@gmail.com>
2021-10-02 10:10:21 +0700

    add test case for get saving list

--- src/test/java/com/alami/cooperation/controller/SavingControllerTests.java
+++ src/test/java/com/alami/cooperation/controller/SavingControllerTests.java
@@ -2,22 +2,30 @@ package com.alami.cooperation.controller;
 
 import com.alami.cooperation.controller.request.TransactionRequest;
 import com.alami.cooperation.dto.TransactionDto;
+import com.alami.cooperation.entity.Saving;
 import com.alami.cooperation.service.SavingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.alami.cooperation.util.ResponseUtil.toJson;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +37,54 @@ public class SavingControllerTests {
 
     @MockBean
     private SavingService savingService;
+
+    private Saving createSavingWawan() throws ParseException {
+        Saving saving = new Saving();
+        saving.setId(1L);
+        saving.setMemberId(1L);
+        saving.setAmount(new BigDecimal(1000000));
+        saving.setCreatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-01"));
+        saving.setUpdatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-01"));
+
+        return saving;
+    }
+
+    private Saving createSavingTeguh() throws ParseException {
+        Saving saving = new Saving();
+        saving.setId(2L);
+        saving.setMemberId(2L);
+        saving.setAmount(new BigDecimal(1250000));
+        saving.setCreatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-01"));
+        saving.setUpdatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-01"));
+
+        return saving;
+    }
+
+    private Saving createSavingJoko() throws ParseException {
+        Saving saving = new Saving();
+        saving.setId(3L);
+        saving.setMemberId(3L);
+        saving.setAmount(new BigDecimal(2000000));
+        saving.setCreatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-02"));
+        saving.setUpdatedDate(new SimpleDateFormat("yyyy-MM-dd").parse("2021-10-02"));
+
+        return saving;
+    }
+
+    @Test
+    public void getSavingList_shouldReturnHttp200_givenValidSavingList() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Saving[] savingArr = new Saving[] {createSavingWawan(), createSavingTeguh(), createSavingJoko()};
+        List<Saving> savingList = Arrays.asList(savingArr);
+        Page<Saving> savingPage = new PageImpl<>(savingList, pageRequest, savingList.size());
+
+        mockMvc.perform(get("/savings?page=0&size=10")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(savingService).getSavingList(pageRequest);
+    }
 
     @Test
     public void createSaving_shouldReturnHttp200_givenValidSaving() throws Exception {
