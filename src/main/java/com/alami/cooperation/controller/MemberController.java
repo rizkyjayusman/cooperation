11774981fe5e8d4115b7d898b66b7b67f73d8155
rizky <rizky.jayusman96@gmail.com>
2021-10-03 20:22:12 +0700

    add member api docs

--- src/main/java/com/alami/cooperation/controller/MemberController.java
+++ src/main/java/com/alami/cooperation/controller/MemberController.java
@@ -2,10 +2,15 @@ package com.alami.cooperation.controller;
 
 import com.alami.cooperation.controller.filter.PaginationFilter;
 import com.alami.cooperation.controller.request.MemberRequest;
+import com.alami.cooperation.controller.response.Response;
 import com.alami.cooperation.dto.MemberDto;
 import com.alami.cooperation.entity.Member;
 import com.alami.cooperation.service.MemberService;
 import com.alami.cooperation.vo.Pagination;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.alami.cooperation.handler.ResponseHandler.success;
 
+@Api(tags = "Member Endpoints")
 @RestController
 @RequestMapping("members")
 public class MemberController {
@@ -27,7 +33,11 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @GetMapping
+    @ApiOperation(value = "Get All Member")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "get all member successfully", response = Response.class),
+    })
+    @GetMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<Object> getMemberList(PaginationFilter paginationFilter) {
         PageRequest pageable = PageRequest.of(paginationFilter.getPage(), paginationFilter.getSize(), Sort.by(Sort.Direction.DESC, "id"));
         Page<Member> page = memberService.getMemberList(pageable);
@@ -35,7 +45,12 @@ public class MemberController {
         return new ResponseEntity<>(pagination, HttpStatus.OK);
     }
 
-    @PostMapping
+    @ApiOperation(value = "Create Member")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "create member successfully", response = Response.class),
+            @ApiResponse(code = 400, message = "invalid parameter", response = Response.class),
+    })
+    @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<Object> createMember(@Validated @RequestBody MemberRequest memberRequest) {
         MemberDto memberDto = new MemberDto();
         memberDto.setFirstName(memberRequest.getFirstName());
