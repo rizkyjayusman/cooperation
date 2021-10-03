8a424f581073a511d8ec5d25131c992465b537a7
rizky <rizky.jayusman96@gmail.com>
2021-10-03 20:17:51 +0700

    add loan docs

--- src/main/java/com/alami/cooperation/controller/LoanController.java
+++ src/main/java/com/alami/cooperation/controller/LoanController.java
@@ -1,14 +1,22 @@
 package com.alami.cooperation.controller;
 
+import com.alami.cooperation.controller.filter.PaginationFilter;
 import com.alami.cooperation.controller.request.TransactionRequest;
+import com.alami.cooperation.controller.response.Response;
 import com.alami.cooperation.dto.TransactionDto;
 import com.alami.cooperation.entity.Loan;
 import com.alami.cooperation.exception.BaseException;
 import com.alami.cooperation.service.LoanService;
 import com.alami.cooperation.vo.Pagination;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static com.alami.cooperation.handler.ResponseHandler.success;
 
+@Api(tags = "Loan Endpoints")
 @RestController
 @RequestMapping("loans")
 public class LoanController {
@@ -23,15 +32,24 @@ public class LoanController {
     @Autowired
     private LoanService loanService;
 
-    @GetMapping
-    public ResponseEntity<Object> getLoanList(Pageable pageable) {
+    @ApiOperation(value = "Get All Deposit")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "get all loan successfully", response = Response.class),
+    })
+    @GetMapping(produces = "application/json", consumes = "application/json")
+    public ResponseEntity<Object> getLoanList(PaginationFilter paginationFilter) {
+        PageRequest pageable = PageRequest.of(paginationFilter.getPage(), paginationFilter.getSize(), Sort.by(Sort.Direction.DESC, "id"));
         Page<Loan> page = loanService.getLoanList(pageable);
         Pagination pagination = new Pagination(page.getContent(), page.getNumber(), page.getTotalElements(), page.getTotalPages());
         return new ResponseEntity<>(pagination, HttpStatus.OK);
     }
 
-
-    @PostMapping
+    @ApiOperation(value = "Create Loan")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "create loan successfully", response = Response.class),
+            @ApiResponse(code = 400, message = "invalid parameter", response = Response.class),
+    })
+    @PostMapping(produces = "application/json", consumes = "application/json")
     public ResponseEntity<Object> createLoanTransaction(@Validated @RequestBody TransactionRequest transactionRequest) throws BaseException {
         TransactionDto transactionDto = new TransactionDto();
         transactionDto.setAmount(transactionRequest.getAmount());
