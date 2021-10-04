d009d105413bc9a1087073f0210f0df371f817f3
rizky <rizky.jayusman96@gmail.com>
2021-10-04 07:43:42 +0700

    remove trxconsumer and println log on transcation publisher

--- src/main/java/com/alami/cooperation/publisher/TransactionPublisher.java
+++ src/main/java/com/alami/cooperation/publisher/TransactionPublisher.java
@@ -1,8 +1,6 @@
 package com.alami.cooperation.publisher;
 
-import com.alami.cooperation.consumer.TransactionConsumer;
 import com.alami.cooperation.dto.TransactionDto;
-import com.alami.cooperation.entity.Transaction;
 import com.alami.cooperation.util.KafkaPublisherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +13,10 @@ import static com.alami.cooperation.util.ResponseUtil.toJson;
 public class TransactionPublisher {
 
     @Autowired
-    private TransactionConsumer transactionConsumer;
-
-    @Autowired
     private KafkaPublisherService kafkaPublisherService;
 
     public void publish(TransactionDto transactionDto) {
         log.info("publish transaction dto into event: {}", toJson(transactionDto));
-        System.out.println("PUBLISH OBJECT THROUGH TRANSACTION PUBLISHER");
         kafkaPublisherService.sendMessage("transaction_detail", toJson(transactionDto));
     }
 }
